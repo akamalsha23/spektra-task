b0e8790f2ad0355a3efb2876fedd73722be36d46
Adil Kamalsha <akamalsha23@outlook.com>
2019-09-18 09:00:45 +0530

    edited

--- ARM_template/install_apache.sh
+++ ARM_template/install_apache.sh
@@ -2,10 +2,10 @@
 apt-get -y update
 
 # install Apache2
-apt-get -y install apache2
+apt-get -y install apache2 
 
 # write some HTML
-echo \<center\>\<h1\>Hello SpektraSystems!\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+echo \<center\>\<h1\>Hello SpekraSystems!\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
 
 # restart Apache
 apachectl restart
